b63bdc934ffce93f3c43cd201d4d755a7a97ede0
AnjalaD <anjaladilhara@gmail.com>
2022-01-02 09:55:46 +0530

    Add error message

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle, Box, Container, Fade } from "@mui/material";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -8,39 +9,66 @@ import StudentsView from "./views/StudentsView";
 function App() {
   const [data, setData] = useState([]);
   const [lastUpdated, setLastUpdated] = useState();
+  const [error, setError] = useState();
 
-  const fetchData = () => {
-    fetch(
-      "https://raw.githubusercontent.com/kdsuneraavinash/cf-timetable-json/master/data.json",
-      { cache: "no-store" }
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        setData(res);
-        setLastUpdated(moment().format("YYYY-MM-DD HH:mm:ss"));
-      });
+  const closeError = () => {
+    setError();
   };
 
   useEffect(() => {
+    const fetchData = () => {
+      fetch(
+        "https://raw.githubusercontent.com/kdsuneraavinash/cf-timetable-json/master/data.json",
+        { cache: "no-store" }
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setError();
+          setData(res);
+          setLastUpdated(moment().format("YYYY-MM-DD HH:mm:ss"));
+        })
+        .catch(() => {
+          setError(
+            `Failed to fetch data at: ${moment().format("YYYY-MM-DD HH:mm:ss")}
+            <br/>
+            <strong>Please check you internet connection</strong>`
+          );
+        });
+    };
+
     fetchData();
     const t = setInterval(fetchData, 30000);
     return () => clearInterval(t);
   }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route
-          path="/students"
-          element={<StudentsView data={data} lastUpdated={lastUpdated} />}
-        />
-        <Route
-          path="/company"
-          element={<CompanyView data={data} lastUpdated={lastUpdated} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {error && (
+        <Fade in>
+          <Box sx={{ position: "fixed", bottom: 10, left: 0, width: 1 }}>
+            <Container maxWidth="sm">
+              <Alert severity="error" onClose={closeError}>
+                <AlertTitle>Error</AlertTitle>
+                <span dangerouslySetInnerHTML={{ __html: error }} />
+              </Alert>
+            </Container>
+          </Box>
+        </Fade>
+      )}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route
+            path="/students"
+            element={<StudentsView data={data} lastUpdated={lastUpdated} />}
+          />
+          <Route
+            path="/company"
+            element={<CompanyView data={data} lastUpdated={lastUpdated} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
